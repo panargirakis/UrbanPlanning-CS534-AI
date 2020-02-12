e3f41c4feb4a5431aaadd3ea72e347176ca8caf7
panargirakis <panargirakis@gmail.com>
2020-02-11 20:46:25 -0500

    Gradle works with input arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,10 @@ public class Main
     // Assignment 1 Google Doc:
     // https://docs.google.com/document/d/1rH2Cmk5KzQ7_EN9-3wJejrr8PcwbdFwpYUoCDd6_OPU/edit
     public static void main(String[] args){
+
         // parameters are the file to read in, and the technique to use - HC and GA
-        String inputFile = args[1];
-        String algorithm = args[2];
+        String inputFile = args[0];
+        String algorithm = args[1];
 
         boolean isUsingHC;
         if (algorithm.equals("HC")) {
@@ -22,8 +23,6 @@ public class Main
         }
 
         Options options = new Options(inputFile, isUsingHC);
-
-        System.out.println("Hello, World!");
 
     }
 }
