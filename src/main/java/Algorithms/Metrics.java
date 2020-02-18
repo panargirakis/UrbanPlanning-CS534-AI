8a0fa4a6358457eb97085339472a36b5cd6d0896
panargirakis <panargirakis@gmail.com>
2020-02-18 14:19:10 -0500

    added map in metrics, made terrainType public

--- src/main/java/Algorithms/Metrics.java
+++ src/main/java/Algorithms/Metrics.java
@@ -1,8 +1,21 @@
 package Algorithms;
 
+import Map.UrbanMap;
+
 public class Metrics {
-    int maxScore;
-    int timeAchieved;
+    private int maxScore;
+    private int timeAchieved;
+    private UrbanMap map;
+
+    public Metrics(int maxScore, int timeAchieved, UrbanMap map) {
+        this.maxScore = maxScore;
+        this.timeAchieved = timeAchieved;
+        this.map = map;
+    }
+
+    public UrbanMap getMap() {
+        return map;
+    }
 
     public int getMaxScore() {
         return maxScore;
