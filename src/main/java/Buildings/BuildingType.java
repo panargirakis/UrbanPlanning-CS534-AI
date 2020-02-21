f77980f31ffc628f5cf6f11a7d71ec38556c49ea
panargirakis <panargirakis@gmail.com>
2020-02-21 11:07:41 -0500

    implemented getting string representation of map

--- src/main/java/Buildings/BuildingType.java
+++ src/main/java/Buildings/BuildingType.java
@@ -1,16 +1,23 @@
 package Buildings;
 
 public enum BuildingType {
-    RESIDENTIAL("residential"), COMMERCIAL("commercial"), INDUSTRIAL("industrial"), EMPTY("empty");
+    RESIDENTIAL("residential", "R"), COMMERCIAL("commercial", "C"),
+    INDUSTRIAL("industrial", "I"), EMPTY("empty", "#");
 
     private final String name;
+    private final String symbol;
 
-    BuildingType(String name) {
+    BuildingType(String name, String symbol) {
         this.name = name;
+        this.symbol = symbol;
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
     }
 }
